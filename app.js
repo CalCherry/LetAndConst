b930014736003af6054215c50df73cf81765a98c
cal <cal@DESKTOP-D1CSPSA.localdomain>
2022-12-09 19:31:16 -0500

    First Commit

---
+++ app.js
@@ -0,0 +1,8 @@
+// 1) var allows for reassigment and redeclaration of variables. let does not allow you to redeclare but it does allow you to reassign a variable and it also creates block scope. var also lets you access hoisted variables.
+
+// 2) var allows for reassigment and redeclaration of variables. const does not let you reassign or redeclare a variable. const also creates block scope. var also lets you access hoisted variables.
+
+// 3) with let you can reassign a variable. with const you cannot reassign or redeclare variables.
+
+// 4) hoisting is a way to describe how the javascript compiler works. variables are placed at the top of whatever scope they are declared in. if you use var, you can access the name and undefined value of a variable before it is later used.
+//      fucntion declarations are also hoisted. 
